b0937681a38474b6ddaa68c1f8101d72416f6443
MariyaN23 <sellaite505@gmail.com>
2023-09-02 16:41:50 +0300

    uncontrolledComponents

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,26 @@ import Accordion from "./modules/accordion/Accordion";
 import {Rating, Rating as R} from './modules/Rating/Rating';
 import {OnOff} from './modules/onOff/OnOff';
 import UncontrolledAccordion from './modules/uncontrolledAccordion/UncontrolledAccordion';
-import {UncontrolledRating} from './modules/UncontrolledRating/Rating';
+import {UncontrolledRating} from './modules/UncontrolledRating/UncontrolledRating';
 
 function App() {
     console.log('App rendering')
     return (
-        <div>
+        <div className={'App'}>
             <OnOff />
             <OnOff />
             <OnOff />
             <UncontrolledAccordion titleValue={"Меню"} />
             <UncontrolledAccordion titleValue={"Друзья"} />
             <UncontrolledRating />
-            {/*<Accordion titleValue={"Меню"} collapsed={true}/>
+            <Accordion titleValue={"Меню"} collapsed={true}/>
             <Accordion titleValue={"Друзья"} collapsed={false}/>
             <Rating value={0}/>
             <Rating value={1}/>
             <Rating value={2}/>
             <Rating value={3}/>
             <Rating value={4}/>
-            <Rating value={5}/>*/}
+            <Rating value={5}/>
 
         </div>
     );
--- src/modules/Rating/Rating.tsx
+++ src/modules/Rating/Rating.tsx
@@ -22,11 +22,8 @@ type StarPropsType = {
 }
 
 function Star(props: StarPropsType) {
-    console.log('Star rendering')
-    if (props.selected) {
-        return <span><b>star</b></span>
-    } else {
-        return <span>star</span>
-    }
+    return (
+        (props.selected) ? <span><b>star</b></span> : <span>star</span>
+    )
 }
 
